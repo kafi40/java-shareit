263f5e9363d0ff576bb12a1c786c738f302f3778
fikalinichev <k.f.i@ya.ru>
2024-11-01 18:03:43 +0300

    Покрыть код тестами

--- server/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -37,6 +37,7 @@ public class ItemControllerTest {
     private final Long userId = 1L;
     private final ItemResponse itemResponse = ModelFactory.createItemResponse(itemId);
     private final CommentResponse commentResponse = ModelFactory.createCommentResponse(itemId);
+
     @Test
     void testGetItem() throws Exception {
         when(itemService.getItem(itemId)).thenReturn(itemResponse);
